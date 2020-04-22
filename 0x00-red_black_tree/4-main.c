69897f02f7498ee5507afe3d7ac57fec20e9f71e
Arthur Damm <indigoarthur@gmail.com>
2020-04-22 04:15:14 +0000

    Rought draft

--- 0x00-red_black_tree/4-main.c
+++ 0x00-red_black_tree/4-main.c
@@ -1,7 +1,17 @@
 #include <stdlib.h>
 #include "rb_trees.h"
+#include <limits.h>
+#include <time.h>
 
 void rb_tree_print(const rb_tree_t *tree);
+rb_tree_t *valid_rb_1(void);
+rb_tree_t *test_case_2(int **_arr, int *_n);
+rb_tree_t *test_case_3(int **_arr, int *_n);
+rb_tree_t *test_case_4(int **_arr, int *_n);
+rb_tree_t *test_case_4b(int **_arr, int *_n);
+int binary_tree_is_bst(const rb_tree_t *tree);
+int *rand_array(int len, int max);
+rb_tree_t *test_random_array(int len, int max, int *_n, int **array);
 
 /**
  * main - Entry point
@@ -11,26 +21,248 @@ void rb_tree_print(const rb_tree_t *tree);
 int main(void)
 {
 	rb_tree_t *tree;
-	int array[] = {
+	/* int array[] = {
 		79, 47, 68, 87, 84, 91, 21, 32, 34, 2,
 		20, 22
+	}; */
+	int array[] = {
+		10, 6, 5, 11
 	};
-	ssize_t n = sizeof(array) / sizeof(array[0]);
+	int *arr = array;
+	int n = sizeof(array) / sizeof(array[0]);
 
-	tree = array_to_rb_tree(array, n);
+	/* tree = array_to_rb_tree(array, n); */
+	/* tree = valid_rb_1(); */
+	tree = test_random_array(40, 50, &n, &arr);
+	/* tree = test_case_2(&arr, &n); */
+	/* tree = test_case_3(&arr, &n); */
+	/* tree = test_case_4(&arr, &n); */
+	/* tree = test_case_4b(&arr, &n); */
 	if (!tree)
 		return (1);
 	rb_tree_print(tree);
+	if (!rb_tree_is_valid(tree))
+	{
+		printf("Error tree invalid RB!\n");
+		return (0);
+	}
+	else if (!binary_tree_is_bst(tree))
+	{
+		printf("Error tree invalid BST!\n");
+		return (0);
+	}
+	else
+	{
+		printf("\nVALID TREE\n\n");
+	}
+	fflush(stdout);
 	for (n--; n >= 0; n--)
 	{
-		tree = rb_tree_remove(tree, array[n]);
+		tree = rb_tree_remove(tree, arr[n]);
 		rb_tree_print(tree);
-		printf("REMOVED [%d]\n", array[n]);
+		printf("REMOVED [%d]\n", arr[n]);
 		if (!rb_tree_is_valid(tree))
 		{
-			printf("Error tree invalid!\n");
+			printf("Error tree invalid RB!\n");
+			return (0);
+		}
+		else if (!binary_tree_is_bst(tree))
+		{
+			printf("Error tree invalid BST!\n");
 			return (0);
 		}
 	}
 	return (0);
+}
+
+/**
+ * valid_rb_1 - Builds a valid R-B tree
+ *
+ * Return: A pointer to the created tree
+ */
+rb_tree_t *valid_rb_1(void)
+{
+	rb_tree_t *root;
+
+	root = rb_tree_node(NULL, 10, BLACK);
+	root->left = rb_tree_node(root, 5, BLACK);
+	root->right = rb_tree_node(root, 11, BLACK);
+	root->left->left = rb_tree_node(root->left, 4, RED);
+	root->left->right = rb_tree_node(root->left, 6, RED);
+	return (root);
+}
+
+rb_tree_t *test_case_1(void)
+{
+	rb_tree_t *root;
+
+	root = rb_tree_node(NULL, 10, BLACK);
+	root->left = rb_tree_node(root, 5, BLACK);
+	root->right = rb_tree_node(root, 11, BLACK);
+	root->left->left = rb_tree_node(root->left, 4, RED);
+	root->left->right = rb_tree_node(root->left, 6, RED);
+	return (root);
+}
+
+/*
+
+*/
+rb_tree_t *test_case_2(int **_arr, int *_n)
+{
+	rb_tree_t *root;
+	static int arr[] = {10};
+
+	*_arr = arr;
+	*_n = sizeof(arr) / sizeof(int);
+	root = rb_tree_node(NULL, 10, BLACK);
+	root->left = rb_tree_node(root, 5, BLACK);
+	root->right = rb_tree_node(root, 11, BLACK);
+	root->right->right = rb_tree_node(root->left, 15, RED);
+	return (root);
+}
+
+/*
+	           .--------------B(040)---.
+         .--R(021)---------.        B(043)
+   .--B(009)         .--B(033)
+R(001)            R(026)
+REMOVE 9
+*/
+rb_tree_t *test_case_3(int **_arr, int *_n)
+{
+	rb_tree_t *root;
+	static int arr[] = {9};
+
+	*_arr = arr;
+	*_n = sizeof(arr) / sizeof(int);
+	root = rb_tree_node(NULL, 40, BLACK);
+	root->left = rb_tree_node(root, 21, RED);
+	root->right = rb_tree_node(root, 43, BLACK);
+	root->left->left = rb_tree_node(root->left, 9, BLACK);
+	root->left->left->left = rb_tree_node(root->left->left, 1, RED);	
+	root->left->right = rb_tree_node(root->left, 33, BLACK);
+	root->left->right->left = rb_tree_node(root->left->right, 26, RED);
+	
+	return (root);
+}
+
+/*
+         .--B(017)---------------.
+   .--B(008)         .--------R(028)---.
+R(001)            B(021)---.        B(045)
+                        R(027)
+TRY RM [45]
+*/
+rb_tree_t *test_case_4(int **_arr, int *_n)
+{
+	rb_tree_t *root;
+	static int arr[] = {45};
+
+	*_arr = arr;
+	*_n = sizeof(arr) / sizeof(int);
+	root = rb_tree_node(NULL, 17, BLACK);
+	root->left = rb_tree_node(root, 8, BLACK);
+	root->right = rb_tree_node(root, 28, RED);
+	root->left->left = rb_tree_node(root->left, 1, RED);
+	root->right->left = rb_tree_node(root->right, 21, BLACK);
+	root->right->right = rb_tree_node(root->right, 45, BLACK);
+	root->right->left->right = rb_tree_node(root->right->left, 27, RED);
+	
+	return (root);
+}
+
+/*
+         .--B(017)---------------.
+   .--B(008)         .--------R(028)---.
+R(001)            B(021)---.        B(045)
+                        R(027)
+TRY RM [45]
+*/
+rb_tree_t *test_case_4b(int **_arr, int *_n)
+{
+	rb_tree_t *root;
+	static int arr[] = {21};
+
+	*_arr = arr;
+	*_n = sizeof(arr) / sizeof(int);
+	root = rb_tree_node(NULL, 17, BLACK);
+	root->left = rb_tree_node(root, 8, BLACK);
+	root->right = rb_tree_node(root, 28, RED);
+	root->left->left = rb_tree_node(root->left, 1, RED);
+	root->right->left = rb_tree_node(root->right, 21, BLACK);
+	root->right->right = rb_tree_node(root->right, 45, BLACK);
+	root->right->right->right = rb_tree_node(root->right->right, 47, RED);
+	
+	return (root);
+}
+
+rb_tree_t *test_random_array(int len, int max, int *_n, int **array)
+{
+	rb_tree_t *root = NULL;
+
+	int *arr = rand_array(len, max);
+	int n;
+
+	for (n = 0; n < len; n++)
+	{
+		printf("INSERTING [%d]\n", arr[n]);
+		rb_tree_insert(&root, arr[n]);
+	}
+		
+	*_n = len;
+	*array = arr;
+	return (root);
+}
+
+/**
+ * is_bst - helper func for binary_tree_is_bst
+ * @tree: pointer to tree node
+ * @min: minimum value of BST tree
+ * @max: maximum value of BST tree
+ * Return: true or false
+ */
+int is_bst(const rb_tree_t *tree, int min, int max)
+{
+	if (!tree)
+		return (1);
+	if (tree->n < min || tree->n > max)
+		return (0);
+	return (is_bst(tree->left, min, tree->n - 1) &&
+		is_bst(tree->right, tree->n + 1, max));
+}
+
+
+/**
+ * binary_tree_is_bst - checks if a binary tree is a valid Binary Search Tree
+ * @tree: pointer to root of tree
+ * Return: 1 if true 0 if false
+ */
+int binary_tree_is_bst(const rb_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (is_bst(tree, INT_MIN, INT_MAX));
+}
+
+/**
+ * rand_array - generates random int array
+ *
+ * @len: the length of the array
+ * @max: the max size of each integer
+ * 
+ * Return: the generated int array
+ */
+int *rand_array(int len, int max)
+{
+	int *arr;
+
+	arr = calloc(len, sizeof(int));
+	if (!arr)
+		return (NULL);
+	if (!max)
+		max = RAND_MAX;
+	srand(time(NULL));
+	while (len--)
+		arr[len] = rand() % max;
+	return (arr);
 }
--- 0x00-red_black_tree/4-rb_tree_remove.c
+++ 0x00-red_black_tree/4-rb_tree_remove.c
@@ -4,6 +4,7 @@ rb_tree_t *rb_tree_remove(rb_tree_t *root, int n)
 {
 	rb_tree_t *node = root;
 
+	printf("TRY RM [%d]\n", n);
 	while (node)
 	{
 		if (n > node->n)
@@ -21,7 +22,7 @@ rb_tree_t *rb_tree_remove(rb_tree_t *root, int n)
 
 void rb_tree_remove_node(rb_tree_t **tree, rb_tree_t *z)
 {
-	rb_tree_t *x, *y = z;
+	rb_tree_t *x, *x_parent = NULL, *y = z;
 	int y_color = y->color;
 
 	printf("RM NODE [%d]\n", z->n);
@@ -29,12 +30,14 @@ void rb_tree_remove_node(rb_tree_t **tree, rb_tree_t *z)
 	{
 		printf("RM CASE 1\n");
 		x = z->right;
+		x_parent = z->parent;
 		rb_tree_replace(tree, z, z->right);
 	}
 	else if (!z->right)
 	{
 		printf("RM CASE 2\n");
 		x = z->left;
+		x_parent = z->parent;
 		rb_tree_replace(tree, z, z->left);
 	}
 	else
@@ -45,9 +48,16 @@ void rb_tree_remove_node(rb_tree_t **tree, rb_tree_t *z)
 			y = y->left;
 		y_color = y->color;
 		x = y->right;
-		if (y->parent != z)
+		if (y->parent == z)
+		{
+			if (x)
+				x->parent = y;
+			x_parent = y;
+		}
+		else
 		{
 			rb_tree_replace(tree, y, y->right);
+			x_parent = y->parent;
 			y->right = z->right;
 			y->right->parent = y;
 		}
@@ -56,90 +66,137 @@ void rb_tree_remove_node(rb_tree_t **tree, rb_tree_t *z)
 		y->left->parent = y;
 		y->color = z->color;
 	}
+	rb_tree_print(*tree);
+	printf("y is [%d] x is [%d] x_parent is [%d]\n", y->n, x ? x->n : 0, x_parent ? x_parent->n : 0);
 	if (y_color == BLACK)
-		rb_tree_remove_fixup(tree, x);
+		rb_tree_remove_fixup(tree, x, x_parent);
 	free(z);
 }
 
-void rb_tree_remove_fixup(rb_tree_t **tree, rb_tree_t *x)
-{
-	if (!x)
-	{
-		printf("X IS NULL!\n");
-		return;
-	}
-	
-	while (x != *tree && x->color == BLACK)
-		if (x == x->parent->left)
-			x = rb_tree_fix_right_sibling(tree, x);
-		else
-			x = rb_tree_fix_left_sibling(tree, x);
-	x->color = BLACK;
+void rb_tree_remove_fixup(rb_tree_t **tree, rb_tree_t *x, rb_tree_t *x_parent)
+{	
+	while ((x && x != *tree && x->color == BLACK) || (!x && x_parent))
+		if ((x && x == x->parent->left) || (!x && x_parent && x_parent->right))
+		{
+			x = rb_tree_fix_right_sibling(tree, x, x_parent);
+			x_parent = NULL;
+		}
+		else if ((x && x == x->parent->right) || (x_parent && x_parent->left))
+		{
+			x = rb_tree_fix_left_sibling(tree, x, x_parent);
+			x_parent = NULL;
+		}
+	if (x)
+		x->color = BLACK;
 }
 
-rb_tree_t *rb_tree_fix_right_sibling(rb_tree_t **tree, rb_tree_t *x)
+rb_tree_t *rb_tree_fix_right_sibling(rb_tree_t **tree, rb_tree_t *x, rb_tree_t *x_parent)
 {
-	rb_tree_t *w = x->parent->right;
+	rb_tree_t *w;
 
+	if (!x_parent)
+		x_parent = x->parent;
+	w = x_parent->right;
+
+	printf("FIX RIGHT SIBLING:\n");
+	printf("X_PARENT is [%d] X\n", x_parent->n);
+	printf("X is [%d]\n", x ? x->n : 0);
+	printf("X parent is [%d]\n", x ? x->parent->n : 0);
+	printf("X_PARENT->right is [%d] X\n", x_parent && x_parent->right ? x_parent->right->n : 0);
 	if (w->color == RED)
 	{
 		w->color = BLACK;
-		x->parent->color = RED;
-		left_rotate(tree, x->parent);
-		w = x->parent->right;
+		x_parent->color = RED;
+		left_rotate(tree, x_parent);
+		w = x_parent->right;
 	}
-	if (w->left->color == BLACK && w->right->color == BLACK)
+	if (!w)
+		return (x);
+	if ((!w->left || w->left->color == BLACK) &&
+		(!w->right || w->right->color == BLACK))
 	{
 		w->color = RED;
-		x = x->parent;
+		x = x_parent;
 	}
 	else
 	{
-		if (w->right->color == BLACK)
+		if (!w->right || w->right->color == BLACK)
 		{
+			printf("CASE 3\n");
 			w->left->color = BLACK;
 			w->color = RED;
-			left_rotate(tree, w);
-			w = x->parent->right;
+			right_rotate(tree, w);
+			w = x_parent->right;
 		}
-		w->color = x->parent->color;
-		x->parent->color = BLACK;
-		w->right->color = BLACK;
-		left_rotate(tree, x->parent);
+		printf("CASE 4\n");
+		w->color = x_parent->color;
+		x_parent->color = BLACK;
+		if (w->right)
+			w->right->color = BLACK;
+		left_rotate(tree, x_parent);
 		x = *tree;
 	}
 	return (x);
 }
 
-rb_tree_t *rb_tree_fix_left_sibling(rb_tree_t **tree, rb_tree_t *x)
+rb_tree_t *rb_tree_fix_left_sibling(rb_tree_t **tree, rb_tree_t *x, rb_tree_t *x_parent)
 {
-	rb_tree_t *w = x->parent->left;
+	rb_tree_t *w;
 
+	if (!x_parent)
+		x_parent = x->parent;
+	w = x_parent->left;
+
+	printf("FIX LEFT SIBLING:\n");
+	printf("X_PARENT is [%d] X\n", x_parent->n);
+	printf("X is [%d]\n", x ? x->n : 0);
+	printf("X parent is [%d]\n", x ? x->parent->n : 0);
+	printf("X_PARENT->left is [%d] X\n", x_parent && x_parent->left ? x_parent->left->n : 0);
 	if (w->color == RED)
 	{
 		w->color = BLACK;
-		x->parent->color = RED;
-		right_rotate(tree, x->parent);
-		w = x->parent->left;
+		x_parent->color = RED;
+		right_rotate(tree, x_parent);
+		w = x_parent->left;
 	}
-	if (w->left->color == BLACK && w->right->color == BLACK)
+	if ((!w->left || w->left->color == BLACK) &&
+		(!w->right || w->right->color == BLACK))
 	{
 		w->color = RED;
-		x = x->parent;
+		x = x_parent;
 	}
 	else
 	{
-		if (w->left->color == BLACK)
+		if (!w->left || w->left->color == BLACK)
 		{
+			printf("CASE 3\n");
+			w->right->color = BLACK;
+			w->color = RED;
+			left_rotate(tree, w);
+			w = x_parent->left;
+		}
+		printf("CASE 4\n");
+		if (w)
+			w->color = x_parent->color;
+		x_parent->color = BLACK;
+		if (w && w->left)
+			w->left->color = BLACK;
+		right_rotate(tree, x_parent);
+		x = *tree;
+		/* if (!w->left || w->left->color == BLACK)
+		{
+			printf("CASE 3\n");
 			w->right->color = BLACK;
 			w->color = RED;
 			right_rotate(tree, w);
-			w = x->parent->left;
+			w = x_parent->left;
 		}
-		w->color = x->parent->color;
-		x->parent->color = BLACK;
-		w->left->color = BLACK;
-		right_rotate(tree, x->parent);
+		printf("CASE 4\n");
+		w->color = x_parent->color;
+		x_parent->color = BLACK;
+		if (w->left)
+			w->left->color = BLACK;
+		right_rotate(tree, x_parent); */
 		x = *tree;
 	}
 	return (x);
