66029afed3e7b4eac744c33c213f5846e5a5d799
Arthur Damm <indigoarthur@gmail.com>
2020-05-30 22:30:54 +0000

    add graphs.h

--- 0x01-graphs/graphs.h
+++ 0x01-graphs/graphs.h
@@ -70,9 +70,5 @@ typedef struct graph_s
 void graph_display(const graph_t *graph);
 graph_t *graph_create(void);
 vertex_t *graph_add_vertex(graph_t *graph, const char *str);
-int graph_add_edge(graph_t *graph, const char *src, const char *dest, edge_type_t type);
-void graph_delete(graph_t *graph);
-size_t depth_first_traverse(const graph_t *graph, void (*action)(const vertex_t *v, size_t depth));
-size_t breadth_first_traverse(const graph_t *graph, void (*action)(const vertex_t *v, size_t depth));
 
 #endif
