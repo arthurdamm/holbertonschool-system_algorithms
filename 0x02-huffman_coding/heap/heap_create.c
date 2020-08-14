8b6b43b01378ac11102ce9b2370f89e3ac776c90
Arthur Damm <indigoarthur@gmail.com>
2020-08-14 17:54:44 +0000

    add heap_create.c

---
+++ 0x02-huffman_coding/heap/heap_create.c
@@ -0,0 +1,21 @@
+#include "heap.h"
+
+/**
+ * heap_create - allocates new heap structure
+ * @data_cmp: the comparison function
+ * Return: pointer to heap_s or NULL
+ */
+heap_t *heap_create(int (*data_cmp)(void *, void *))
+{
+	heap_t *heap = NULL;
+
+	if (!data_cmp)
+		return (NULL);
+	heap = malloc(sizeof(heap));
+	if (!heap)
+		return (NULL);
+	heap->root = NULL;
+	heap->size = 0;
+	heap->data_cmp = data_cmp;
+	return (heap);
+}
