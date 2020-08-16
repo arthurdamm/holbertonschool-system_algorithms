9e8760a2fcb4d374cd3fa6b4d4116a9ad147ff88
Arthur Damm <indigoarthur@gmail.com>
2020-08-16 00:33:44 +0000

    add huffman_tree.c

--- 0x02-huffman_coding/huffman.h
+++ 0x02-huffman_coding/huffman.h
@@ -21,5 +21,6 @@ int data_cmp(void *node_a, void *node_b);
 void free_data(void *data);
 heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size);
 int huffman_extract_and_insert(heap_t *priority_queue);
+binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size);
 
 #endif
---
+++ 0x02-huffman_coding/huffman_tree.c
@@ -0,0 +1,24 @@
+#include "huffman.h"
+
+/**
+ * huffman_tree - creates a huffman tree from data
+ * @data: character array
+ * @freq: frequency of char array
+ * @size: size of array args
+ * Return: root node of huffman tree
+ */
+binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size)
+{
+	heap_t *heap = huffman_priority_queue(data, freq, size);
+
+	setbuf(stdout, NULL);
+	if (!heap)
+		return (NULL);
+	while (heap->size > 1)
+	{
+		if (!huffman_extract_and_insert(heap))
+			return (NULL);
+	}
+	return (heap->root->data);
+	free(heap);
+}
