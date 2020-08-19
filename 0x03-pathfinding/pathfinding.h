975aa458280279158a60cde655727edd64ef6c88
Arthur Damm <indigoarthur@gmail.com>
2020-08-19 21:59:24 +0000

    Included header with updated prototypes

--- 0x03-pathfinding/pathfinding.h
+++ 0x03-pathfinding/pathfinding.h
@@ -24,8 +24,8 @@ typedef struct point_s
 
 queue_t *backtracking_array(char **map, int rows, int cols,
 	point_t const *start, point_t const *target);
-int backtrack(char **map, int rows, int cols, point_t const *start,
-	point_t const *target, int x, int y, queue_t *path);
+int backtrack(char **map, int rows, int cols, point_t const *target,
+	int x, int y, queue_t *path);
 
 queue_t *backtracking_graph(graph_t *graph, vertex_t const *start,
 	vertex_t const *target);
